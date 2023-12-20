117725afcdfc2b432914e69c1b42b7ec955102ae
Kareem OSAMA <kareem.osama.part@servier.com>
2023-12-20 10:00:41 +0200

    Solve CartPage Issue

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,11 +1,11 @@
+import { useTitle } from "../../hooks/useTitle";
 import { CartEmpty } from "./components/CartEmpty";
 import { CartList } from "./components/CartList";
 import { useCart } from "../../context";
-import { useTitle } from "../../hooks/useTitle";
 
 export const CartPage = () => {
-  useTitle(`Cart (${cartList.length})`);
   const { cartList } = useCart();
+  useTitle(`Cart (${cartList.length})`);
 
   return <main>{cartList.length ? <CartList /> : <CartEmpty />}</main>;
 };
