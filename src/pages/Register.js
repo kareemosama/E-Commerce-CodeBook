24efdf60a02ce1eb2a8ddde3614ebb207455c16e
Kareem OSAMA <kareem.osama.part@servier.com>
2023-12-20 10:06:34 +0200

    Names

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -44,7 +44,7 @@ export const Register = () => {
             type="name"
             id="name"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="Shubham Sarda"
+            placeholder="Kareem Osama"
             required
             autoComplete="off"
           />
@@ -60,7 +60,7 @@ export const Register = () => {
             type="email"
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="shubham@example.com"
+            placeholder="Kareem@example.com"
             required
             autoComplete="off"
           />
